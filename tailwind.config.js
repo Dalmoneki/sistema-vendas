67720d23aec9500c6574d7ec177cd6972b2ef39b
dalmoneki <dalmoneki@gmail.com>
2024-12-03 11:08:51 -0300

    Correção: Adicionado tailwind.config.js e ajustes no layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx}', // Caminhos para os arquivos que utilizam classes Tailwind
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: '#FFC107', // Cor principal personalizada
-        secondary: '#FFCA28', // Cor secundária personalizada
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
